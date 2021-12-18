1d458aa2dd41fe7294139c80e9bfc30daa17344a
Miguel Gil <mgil@yunbit.es>
2021-12-18 18:22:14 +0100

    Script for generate the Dokerfile for any wordpress version
    Took 1 hour 33 minutes

---
+++ update_dockerfiles.php
@@ -0,0 +1,31 @@
+<?php
+$handle = fopen("tags", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $line = preg_replace('/[^a-zA-Z0-9\.\-\:]/','', $line);
+
+        /*$elem = str_replace("'", '', $line);
+        $elem = str_replace("$\n", '', $elem);*/
+
+        $version = explode(':', $line);
+        $versionElems = explode('-', $version[1]);
+
+        $path = $version[0];
+
+        if ($versionElems) {
+            foreach ($versionElems as $elem) {
+                $path .= '/' . $elem;
+            }
+        }
+
+        if (!is_dir($path) && !mkdir($path,0775, true)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $path));
+        }
+
+        $content = file_get_contents('Dockerfile.template');
+        $content = str_replace('{{ env.wordpressTag }}', $line, $content);
+        file_put_contents($path.'/Dockerfile', $content);
+    }
+
+    fclose($handle);
+}
